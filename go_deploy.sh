ff8c25c2abd8d0d55a637123d1d9b5606fa32c2c
yangjiafeng <yang.yjf@aliyun.com>
2022-10-23 23:57:58 +0800

    .gitignore

--- go_deploy.sh
+++ go_deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-curl http://laughing.plus:9527/stop
+curl http://laughing.plus:x/stop
 GOOS=linux GOARCH=amd64 go build -o laughing main.go
-sshpass -p '' scp laughing root@43.135.79.57:/usr/local/src
-sshpass -p '' scp run.sh root@43.135.79.57:/usr/local/src
-sshpass -p '' scp stop.sh root@43.135.79.57:/usr/local/src
+sshpass -p '' scp laughing root@43.135.x.x:/usr/local/src
+sshpass -p '' scp run.sh root@43.135.x.x:/usr/local/src
+sshpass -p '' scp stop.sh root@43.135.x.x:/usr/local/src
 echo "scp file to remote successfuly!"
 rm -rf laughing
-curl http://laughing.plus:9527/run
+curl http://laughing.plus:x/run
 echo "deploy laughing complete！"
--- web_deploy.sh
+++ web_deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-curl http://laughing.plus:9527/removeWeb &&
-curl http://laughing.plus:9527/stop &&
-sshpass -p '' scp -r web/styles root@43.135.79.57:/usr/local/src/web/styles &&
-sshpass -p '' scp -r web/views root@43.135.79.57:/usr/local/src/web/views &&
-curl http://laughing.plus:9527/run &&
+curl http://laughing.plus:x/removeWeb &&
+curl http://laughing.plus:x/stop &&
+sshpass -p '' scp -r web/styles root@43.135.xx.xx:/usr/local/src/web/styles &&
+sshpass -p '' scp -r web/views root@43.135.xx.xx:/usr/local/src/web/views &&
+curl http://laughing.plus:x/run &&
 echo "deploy web complete!"
