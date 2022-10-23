966a45d9cbd4ff595627e4e25cb0bd3ca6af0a04
yangjiafeng <852264915@qq.com>
2022-10-23 23:51:37 +0800

    Update go_deploy.sh

--- go_deploy.sh
+++ go_deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 curl http://laughing.plus:9527/stop
 GOOS=linux GOARCH=amd64 go build -o laughing main.go
-sshpass -p '@YangZhang^&#i>(Le*uUo09*%$C.,' scp laughing root@43.135.79.57:/usr/local/src
-sshpass -p '@YangZhang^&#i>(Le*uUo09*%$C.,' scp run.sh root@43.135.79.57:/usr/local/src
-sshpass -p '@YangZhang^&#i>(Le*uUo09*%$C.,' scp stop.sh root@43.135.79.57:/usr/local/src
+sshpass -p '' scp laughing root@43.135.79.57:/usr/local/src
+sshpass -p '' scp run.sh root@43.135.79.57:/usr/local/src
+sshpass -p '' scp stop.sh root@43.135.79.57:/usr/local/src
 echo "scp file to remote successfuly!"
 rm -rf laughing
 curl http://laughing.plus:9527/run
